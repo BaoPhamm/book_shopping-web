924705bd45a342e7be95acc645c092831b0b7a6d
BaoPhamm <phambao1871997@gmail.com>
2022-04-11 17:15:53 +0700

    add model books

---
+++ back_end/src/main/java/com/springboot/shopping/model/Books.java
@@ -0,0 +1,108 @@
+package com.springboot.shopping.model;
+
+import java.sql.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Books {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+
+	private String title;
+	private String author;
+	private long total_pages;
+	private long required_age;
+	private Date release_date;
+	private Double price;
+	private String img_src;
+
+	public Books() {
+		super();
+	}
+
+	public Books(long id, String title, String author, long total_pages, long required_age, Date release_date,
+			Double price, String img_src) {
+		super();
+		this.id = id;
+		this.title = title;
+		this.author = author;
+		this.total_pages = total_pages;
+		this.required_age = required_age;
+		this.release_date = release_date;
+		this.price = price;
+		this.img_src = img_src;
+	}
+
+	public long getId() {
+		return id;
+	}
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+	public String getTitle() {
+		return title;
+	}
+
+	public void setTitle(String title) {
+		this.title = title;
+	}
+
+	public String getAuthor() {
+		return author;
+	}
+
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+
+	public long getTotal_pages() {
+		return total_pages;
+	}
+
+	public void setTotal_pages(long total_pages) {
+		this.total_pages = total_pages;
+	}
+
+	public long getRequired_age() {
+		return required_age;
+	}
+
+	public void setRequired_age(long required_age) {
+		this.required_age = required_age;
+	}
+
+	public Date getRelease_date() {
+		return release_date;
+	}
+
+	public void setRelease_date(Date release_date) {
+		this.release_date = release_date;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+
+	public String getImg_src() {
+		return img_src;
+	}
+
+	public void setImg_src(String img_src) {
+		this.img_src = img_src;
+	}
+
+}
