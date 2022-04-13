fb4921eb5f03a311d931fe41137b264c7a0781d5
BaoPhamm <phambao1871997@gmail.com>
2022-04-13 19:49:27 +0700

    Add ApiRequestException

---
+++ back_end/src/main/java/com/springboot/shopping/exception/ApiRequestException.java
@@ -0,0 +1,17 @@
+package com.springboot.shopping.exception;
+
+import org.springframework.http.HttpStatus;
+
+public class ApiRequestException extends RuntimeException {
+	private final HttpStatus status;
+
+	public ApiRequestException(String message, HttpStatus status) {
+		super(message);
+		this.status = status;
+	}
+
+	public HttpStatus getStatus() {
+		return status;
+	}
+
+}
