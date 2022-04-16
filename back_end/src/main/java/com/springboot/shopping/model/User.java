8efedaa1303f59ee10ba8bdd2c85aff5ea10edf1
BaoPhamm <phambao1871997@gmail.com>
2022-04-16 14:23:11 +0700

    Add User model

---
+++ back_end/src/main/java/com/springboot/shopping/model/User.java
@@ -0,0 +1,45 @@
+package com.springboot.shopping.model;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Table(name = "users", uniqueConstraints = { @UniqueConstraint(columnNames = "username"),
+		@UniqueConstraint(columnNames = "phoneNumber") })
+//@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+public class User {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String firstName;
+	private String lastName;
+	private String username;
+	private String password;
+	private String phoneNumber;
+	private String address;
+
+//	@ElementCollection(targetClass = Role.class, fetch = FetchType.EAGER)
+//	@CollectionTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"))
+//	@Enumerated(EnumType.STRING)
+
+	@ManyToMany(fetch = FetchType.EAGER)
+	private Collection<Role> roles = new ArrayList<>();
+
+}
