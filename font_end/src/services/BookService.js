0a7d7b859f6887fce65db2014783e026924a85fc
BaoPhamm <phambao1871997@gmail.com>
2022-04-21 22:05:41 +0700

    Add UserService and BookService

---
+++ font_end/src/services/BookService.js
@@ -0,0 +1,27 @@
+import axios from "axios";
+
+const BOOK_API_BASE_URL = "http://localhost:8080/api/v1/books";
+
+class BookService {
+  getBooks() {
+    return axios.get(BOOK_API_BASE_URL);
+  }
+
+  //   createEmployee(employee) {
+  //     return axios.post(EMPLOYEE_API_BASE_URL, employee);
+  //   }
+
+  //   getEmployeeById(employeeId) {
+  //     return axios.get(EMPLOYEE_API_BASE_URL + "/" + employeeId);
+  //   }
+
+  //   updateEmployee(employee, employeeId) {
+  //     return axios.put(EMPLOYEE_API_BASE_URL + "/" + employeeId, employee);
+  //   }
+
+  //   deleteEmployee(employeeId) {
+  //     return axios.delete(EMPLOYEE_API_BASE_URL + "/" + employeeId);
+  //   }
+}
+
+export default new BookService();
