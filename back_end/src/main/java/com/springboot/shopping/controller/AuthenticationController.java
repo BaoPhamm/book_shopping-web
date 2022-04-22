07a58298902c2db8cf837f0f763ff43b39e3807e
BaoPhamm <phambao1871997@gmail.com>
2022-04-22 19:00:30 +0700

    Remove unused import

--- back_end/src/main/java/com/springboot/shopping/controller/AuthenticationController.java
+++ back_end/src/main/java/com/springboot/shopping/controller/AuthenticationController.java
@@ -1,20 +1,13 @@
 package com.springboot.shopping.controller;
 
-import javax.validation.Valid;
-
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.springboot.shopping.dto.PasswordResetRequest;
 import com.springboot.shopping.dto.auth.AuthenticationRequest;
 import com.springboot.shopping.dto.auth.AuthenticationResponse;
-import com.springboot.shopping.exception.InputFieldException;
 import com.springboot.shopping.mapper.AuthenticationMapper;
 
 import lombok.AllArgsConstructor;
