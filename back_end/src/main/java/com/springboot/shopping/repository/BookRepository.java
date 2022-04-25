0a5cef87916a35d156771e5520e09eae318a5ee0
BaoPhamm <phambao1871997@gmail.com>
2022-04-25 16:08:40 +0700

    Fix typo mistake of nativeQuery

--- back_end/src/main/java/com/springboot/shopping/repository/BookRepository.java
+++ back_end/src/main/java/com/springboot/shopping/repository/BookRepository.java
@@ -12,9 +12,10 @@ import com.springboot.shopping.model.Book;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-	@Query(value = "select b from books b inner join books_categories bc on b.id = bc.books_id "
+	@Query(value = "select * from books b "
+			+ "inner join books_categories bc on b.id = bc.book_id "
 			+ "inner join categories c on c.id = bc.categories_id "
-			+ "where c.name = ?1" ,nativeQuery = true)
+			+ "where c.name = :categoryName" ,nativeQuery = true)
 	List<Book> findByCategory(String categoryName);
 
 	Optional<Book> findByTitle(String bookTitle);
