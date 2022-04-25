6411888f8f8bf10481544d5cb9af9f334350116f
BaoPhamm <phambao1871997@gmail.com>
2022-04-25 16:09:27 +0700

    Remove variables users

--- back_end/src/main/java/com/springboot/shopping/model/Role.java
+++ back_end/src/main/java/com/springboot/shopping/model/Role.java
@@ -1,22 +1,17 @@
 package com.springboot.shopping.model;
 
-import java.util.ArrayList;
-import java.util.Collection;
-
 import javax.persistence.Entity;
-import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.ManyToMany;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
+@NoArgsConstructor
 @Entity
 public class Role {
 
@@ -25,7 +20,4 @@ public class Role {
 	private Long id;
 	private String name;
 
-	@ManyToMany(mappedBy = "roles", fetch = FetchType.LAZY)
-	Collection<UserEntity> users = new ArrayList<>();
-			
 }
