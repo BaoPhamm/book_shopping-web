7e193f818f2beac76ce6dfe67f29f9071cb94a09
BaoPhamm <phambao1871997@gmail.com>
2022-04-25 16:05:47 +0700

    change getmapping to postMapping

--- back_end/src/main/java/com/springboot/shopping/controller/BookController.java
+++ back_end/src/main/java/com/springboot/shopping/controller/BookController.java
@@ -5,12 +5,14 @@ import java.util.List;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.springboot.shopping.dto.book.BookResponse;
 import com.springboot.shopping.mapper.BookMapper;
+import com.springboot.shopping.mapper.CategoryMapper;
 
 import lombok.RequiredArgsConstructor;
 
@@ -34,8 +36,9 @@ public class BookController {
 	}
 	
 	// Get All books
-	@GetMapping("/category")
+	@PostMapping("/category")
 	public ResponseEntity<List<BookResponse>> getBooksByCategory(@RequestBody String categoryName) {
 		return ResponseEntity.ok(bookMapper.findBooksByCategory(categoryName));
 	}
+	
 }
