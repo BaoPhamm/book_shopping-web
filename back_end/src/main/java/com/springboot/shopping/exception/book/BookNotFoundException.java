716b54ab34eb260f251202a4e2d6872080e3b5ce
BaoPhamm <phambao1871997@gmail.com>
2022-05-04 19:09:55 +0700

    Add http status code for book not found exception

--- back_end/src/main/java/com/springboot/shopping/exception/book/BookNotFoundException.java
+++ back_end/src/main/java/com/springboot/shopping/exception/book/BookNotFoundException.java
@@ -1,21 +1,19 @@
 package com.springboot.shopping.exception.book;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import lombok.Getter;
 
 @Getter
+@ResponseStatus(HttpStatus.NOT_FOUND)
 public class BookNotFoundException extends RuntimeException {
-
-	private final HttpStatus status;
 
 	public BookNotFoundException() {
 		super("Book is not found!");
-		this.status = HttpStatus.NOT_FOUND;
 	}
 
 	public BookNotFoundException(Long bookId) {
 		super("Book with id: " + bookId + " is not found!");
-		this.status = HttpStatus.NOT_FOUND;
 	}
 }
