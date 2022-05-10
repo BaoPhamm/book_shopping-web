50eb4bc19f52543e71f21559370bdf56fd17bb43
BaoPhamm <phambao1871997@gmail.com>
2022-05-10 18:44:14 +0700

    Add functions block and unblock user in userAdminController

--- back_end/src/main/java/com/springboot/shopping/controller/admin/UserAdminController.java
+++ back_end/src/main/java/com/springboot/shopping/controller/admin/UserAdminController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -49,6 +50,21 @@ public class UserAdminController {
 	public ResponseEntity<String> deleteUser(@PathVariable("id") Long UserId) {
 		String adminUsername = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
 		String message = userService.deleteUser(UserId, adminUsername);
+		return ResponseEntity.ok(message);
+	}
+
+	// Block an existing user by ID
+	@PutMapping("/block")
+	public ResponseEntity<String> blockUser(@RequestBody Long UserId) {
+		String adminUsername = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+		String message = userService.blockUser(UserId, adminUsername);
+		return ResponseEntity.ok(message);
+	}
+
+	// Unblock an existing user by ID
+	@PutMapping("/unblock")
+	public ResponseEntity<String> unBlockUser(@RequestBody Long UserId) {
+		String message = userService.unBlockUser(UserId);
 		return ResponseEntity.ok(message);
 	}
 
