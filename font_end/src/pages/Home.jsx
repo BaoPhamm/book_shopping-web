4fddc3ce9e56462d7c5d52c15a1d05cbdb82deb0
BaoPhamm <phambao1871997@gmail.com>
2022-05-14 17:04:32 +0700

    Add component Products to Homw page

--- font_end/src/pages/Home.jsx
+++ font_end/src/pages/Home.jsx
@@ -1,12 +1,26 @@
 import React from "react";
 import Slider from "../components/Slider";
 import Categories from "../components/Categories";
+import Products from "../components/Products";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  margin: 20px;
+`;
+const ProductsContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
 
 const Home = () => {
   return (
     <div>
       <Slider />
       <Categories />
+      <ProductsContainer>
+        <Title>All Books</Title>
+        <Products selectedCategory={0} />
+      </ProductsContainer>
     </div>
   );
 };
